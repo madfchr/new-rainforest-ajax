037c50bb0802d1f15c343b7da33a9b282ceacb17
Ariane Deschenes Foucher <ariane.df@me.com>
2016-05-01 23:11:18 -0400

    Implemented an AJAX search. Also fixed a typy in product partial.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   def ensure_logged_in
     unless current_user
       flash[:alert] = "Please log in"
-      redirect_to new_session_url
+      redirect_to new_session_path
     end
   end
 end
