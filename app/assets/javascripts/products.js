f7fa83636cfb149a08c35aaf20cb33a3a09489ae
Ariane Deschenes Foucher <ariane.df@me.com>
2016-05-01 23:36:35 -0400

    Refactored product.js to use the $.ajax() function

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,4 +1,17 @@
 $(document).on('ready page:load', function() {
+  $('search-form').submit(function(event) {
+    event.preventDefault();
+    var searchValue = $('#search').val();
+
+    $ajax({
+      url: 'products?search=' + searchValue,
+      type: 'GET',
+      datatype: 'html'
+    }).done(function(data) {
+      $('#products').html(data);
+    });
+  });
+});
     function display_search_results() {
       if (this.readyState === 4) {
         console.log(this);
