648e1dc77392362e0888e91dc062ef94d341e35e
Ariane Deschenes Foucher <ariane.df@me.com>
2016-05-01 23:47:38 -0400

    Using $.getScript() in product.js, added respond_to to products_controller, and created an index.js.erb template in views.

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -3,11 +3,13 @@ $(document).on('ready page:load', function() {
     event.preventDefault();
     var searchValue = $('#search').val();
 
-    $.get('/products?search=' + searchValue)
-      .done(function(data){
-        console.log(data);
-        $('#products').html(data);
-      });
+    $.getScript('/products?search=' + searchValue);
+
+    
+      // .done(function(data){
+      //   console.log(data);
+      //   $('#products').html(data);
+      // });
     // $ajax({
     //   url: 'products?search=' + searchValue,
     //   type: 'GET',
