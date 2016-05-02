63c315292d17883d2adccb8f8479821769e1d482
Ariane Deschenes Foucher <ariane.df@me.com>
2016-05-01 23:40:28 -0400

    Replaced the $.ajax() function with $.get() function in products.js

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -3,31 +3,36 @@ $(document).on('ready page:load', function() {
     event.preventDefault();
     var searchValue = $('#search').val();
 
-    $ajax({
-      url: 'products?search=' + searchValue,
-      type: 'GET',
-      datatype: 'html'
-    }).done(function(data) {
-      $('#products').html(data);
-    });
+    $.get('/products?search=' + searchValue)
+      .done(function(data){
+        console.log(data);
+        $('#products').html(data);
+      });
+    // $ajax({
+    //   url: 'products?search=' + searchValue,
+    //   type: 'GET',
+    //   datatype: 'html'
+    // }).done(function(data) {
+    //   $('#products').html(data);
+    // });
   });
 });
-    function display_search_results() {
-      if (this.readyState === 4) {
-        console.log(this);
-        document.getElementById('products').innerHTML = this.responseText;
-      }
-    }
-
-    var form = document.getElementById('search-form');
-    form.addEventListener('submit', function(event) {
-      event.preventDefault();
-      var searchValue = document.getElementById('search').value;
-
-      var xhr = new XMLHttpRequest();
-      xhr.onload = display_search_results;
-      xhr.open('GET', '/products?search=' + searchValue, true);
-      xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-      xhr.send();
-    });
-});
+//     function display_search_results() {
+//       if (this.readyState === 4) {
+//         console.log(this);
+//         document.getElementById('products').innerHTML = this.responseText;
+//       }
+//     }
+//
+//     var form = document.getElementById('search-form');
+//     form.addEventListener('submit', function(event) {
+//       event.preventDefault();
+//       var searchValue = document.getElementById('search').value;
+//
+//       var xhr = new XMLHttpRequest();
+//       xhr.onload = display_search_results;
+//       xhr.open('GET', '/products?search=' + searchValue, true);
+//       xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+//       xhr.send();
+//     });
+// });
